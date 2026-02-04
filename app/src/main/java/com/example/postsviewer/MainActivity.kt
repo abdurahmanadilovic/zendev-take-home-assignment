eebe3582bfa96b12d0c426558e4b14550e3bf8cb
Abdurahman <abdurahman.adilovic@gmail.com>
2026-02-04 10:21:34 +0100

    add skeleton for compose navigation

--- app/src/main/java/com/example/postsviewer/MainActivity.kt
+++ app/src/main/java/com/example/postsviewer/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.postsviewer.ui.theme.PostsViewerTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PostsViewerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MyAppNavHost(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +31,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PostListScreen(onNavigateToPostDetails: (postId: Int) -> Unit) {
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PostsViewerTheme {
-        Greeting("Android")
+fun PostDetailsScreen() {
+
+}
+
+@Serializable
+object Home
+
+@Serializable
+data class PostDetailsScreen(val id: Int)
+
+@Composable
+fun MyAppNavHost(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController(),
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = Home
+    ) {
+        composable<PostDetailsScreen> {
+            PostListScreen(
+                onNavigateToPostDetails = { postId -> navController.navigate(route = PostDetailsScreen(id = postId)) },
+                /*...*/
+            )
+        }
+        composable<PostDetailsScreen> { PostDetailsScreen(/*...*/) }
     }
 }
