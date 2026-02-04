fc1abccf17609186893e6d3c4f70249cdae478f6
Abdurahman <abdurahman.adilovic@gmail.com>
2026-02-04 12:55:25 +0100

    extract post card and posts list header composables

--- app/src/main/java/com/example/postsviewer/MainActivity.kt
+++ app/src/main/java/com/example/postsviewer/MainActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
@@ -17,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -26,10 +24,11 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.example.postsviewer.presentation.PostUiState
 import com.example.postsviewer.presentation.PostViewModel
+import com.example.postsviewer.ui.components.PostCard
+import com.example.postsviewer.ui.components.PostsListHeader
 import com.example.postsviewer.ui.theme.PostsViewerTheme
 import kotlinx.serialization.Serializable
 import org.koin.androidx.compose.koinViewModel
-import org.koin.core.definition.indexKey
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,16 +61,17 @@ fun PostListScreen(
         when (val uiState = state) {
             is PostUiState.Loading -> CircularProgressIndicator(Modifier.align(Alignment.Center))
             is PostUiState.Success -> {
-                LazyColumn {
-                    items(uiState.posts.size) { index ->
-                        val post = uiState.posts[index]
-                        Text(
-                            text = post.title,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable { onNavigateToPostDetails(post.id) }
-                                .padding(16.dp)
-                        )
+                Column {
+                    LazyColumn {
+                        item {
+                            PostsListHeader(uiState.posts.size)
+                        }
+                        items(uiState.posts.size) { index ->
+                            val post = uiState.posts[index]
+                            PostCard(post, onClick = {
+                                onNavigateToPostDetails(post.id)
+                            })
+                        }
                     }
                 }
             }
